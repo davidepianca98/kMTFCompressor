7f50f9ecefd2d2eecfb31390974e69a280627b53
Davide Pianca <davidepianca98@gmail.com>
2022-03-28 14:44:20 +0200

    Generic types in block and stream compressor

--- src/MTFBlockWorker.h
+++ src/MTFBlockWorker.h
@@ -13,7 +13,7 @@
 #include "encoders/AdaptiveHuffman.h"
 #include "stream/ibufbitstream.h"
 
-template <typename HASH>
+template <typename HASH, typename T>
 class MTFBlockWorker {
 
     int k;
@@ -29,15 +29,15 @@ class MTFBlockWorker {
     uint32_t compressBlock(const uint8_t *in, int size, uint8_t *final_block) {
         auto *out_block1 = new uint32_t[size];
 
-        MTFHashTableBlock<uint64_t> mtf(k, size, std::ref(hash));
+        MTFHashTableBlock<T> mtf(k, size, std::ref(hash));
         mtf.encode(in, size, out_block1);
 
         obufbitstream buf(final_block, out_block.size());
-        AdaptiveHuffman ah(256 + 8 + 1);
+        AdaptiveHuffman ah(256 + byte_size() + 1);
         for (int i = 0; i < size; i++) {
             ah.encode(out_block1[i], buf);
         }
-        ah.encode(256 + 8, buf); // EOF
+        ah.encode(256 + byte_size(), buf); // EOF
         buf.flush_remaining();
         uint32_t compressed_size = buf.size();
 
@@ -53,21 +53,35 @@ class MTFBlockWorker {
         //uint32_t decompressed_size = FastPForEncoder::decompress(reinterpret_cast<const uint32_t *>(in), size / 4, out_block1);
 
         ibufbitstream buf(in, size);
-        AdaptiveHuffman ah(256 + 8 + 1);
+        AdaptiveHuffman ah(256 + byte_size() + 1);
         uint32_t decompressed_size = 0;
         while (true) {
             out_block1[decompressed_size] = ah.decode(buf);
-            if (out_block1[decompressed_size] == -1 || out_block1[decompressed_size] == 256 + 8) {
+            if (out_block1[decompressed_size] == -1 || out_block1[decompressed_size] == 256 + byte_size()) {
                 break;
             }
             decompressed_size++;
         }
 
-        MTFHashTableBlock<uint64_t> mtf(k, decompressed_size, std::ref(hash));
+        MTFHashTableBlock<T> mtf(k, decompressed_size, std::ref(hash));
         mtf.decode(out_block1, (long) decompressed_size, final_block);
 
         delete[] out_block1;
         return decompressed_size;
+    }
+
+    constexpr static uint8_t byte_size() noexcept {
+        if (std::is_same<T, boost::multiprecision::uint128_t>::value) {
+            return 16;
+        } else if (std::is_same<T, boost::multiprecision::uint256_t>::value) {
+            return 32;
+        } else if (std::is_same<T, boost::multiprecision::uint512_t>::value) {
+            return 64;
+        } else if (std::is_same<T, boost::multiprecision::uint1024_t>::value) {
+            return 128;
+        } else {
+            return sizeof(T);
+        }
     }
 
 public:
@@ -77,7 +91,7 @@ public:
     void startCompression(long size) {
         if (!valid && size > 0) {
             hash = HASH(k, hash_size);
-            future = std::async(std::launch::async, &MTFBlockWorker<HASH>::compressBlock, this, in_block.data(), size, out_block.data());
+            future = std::async(std::launch::async, &MTFBlockWorker<HASH, T>::compressBlock, this, in_block.data(), size, out_block.data());
             valid = true;
         }
     }
@@ -85,7 +99,7 @@ public:
     void startDecompression(long size) {
         if (!valid && size > 0) {
             hash = HASH(k, hash_size);
-            future = std::async(std::launch::async, &MTFBlockWorker<HASH>::decompressBlock, this, in_block.data(), size, out_block.data());
+            future = std::async(std::launch::async, &MTFBlockWorker<HASH, T>::decompressBlock, this, in_block.data(), size, out_block.data());
             valid = true;
         }
     }
--- src/MTFHashCompressor.h
+++ src/MTFHashCompressor.h
@@ -2,17 +2,154 @@
 #ifndef MTF_MTFHASHCOMPRESSOR_H
 #define MTF_MTFHASHCOMPRESSOR_H
 
+#include <iostream>
+#include <fstream>
+#include <random>
+#include "MTFHashCompressor.h"
+#include "MTFBlockWorker.h"
+#include "MTFHashTableStream.h"
+#include "RabinKarp.h"
+#include "Identity.h"
+#include "stream/ofbitstream.h"
+#include "stream/ifbitstream.h"
 
 class MTFHashCompressor {
 
 public:
-    static int compress_block(const std::string& path, const std::string& out_path, int k);
+    static int get_cores() {
+        int processor_count = (int) std::thread::hardware_concurrency();
+        if (processor_count == 0) {
+            processor_count = 1;
+        }
+        return processor_count;
+    }
 
-    static int compress_stream(const std::string& path, const std::string& out_path, int k);
+    template <typename HASH, typename T>
+    static int compress_block(const std::string& path, const std::string& out_path, int k) {
+        std::ifstream in_file(path, std::ios::binary);
+        if (in_file.fail()) {
+            return 1;
+        }
+        std::ofstream out_file(out_path, std::ios::binary);
 
-    static int decompress_block(const std::string& path, const std::string& out_path, int k);
+        int core_number = get_cores() - 2;
 
-    static int decompress_stream(const std::string& path, const std::string& out_path, int k);
+        //int block_size = 1024 * 1024; // 1 MB block size
+
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution<int> dist(1, 4);
+
+        std::vector<MTFBlockWorker<HASH, T>> workers;
+        for (int i = 0; i < core_number; i++) {
+            int block_size = dist(mt) * 1024 * 1024;
+            workers.emplace_back(k, 4095, block_size, block_size * 4 + 1024);
+        }
+
+        while (in_file.good()) {
+            for (MTFBlockWorker<HASH, T>& worker: workers) {
+                in_file.read(reinterpret_cast<char *>(worker.get_in_block()), worker.get_in_block_size());
+                long read_bytes = in_file.gcount();
+
+                worker.startCompression(read_bytes);
+            }
+
+            for (MTFBlockWorker<HASH, T>& worker: workers) {
+                uint32_t compressed_block_size = worker.get();
+                if (compressed_block_size > 0) {
+                    out_file.write(reinterpret_cast<const char *>(&compressed_block_size), 4);
+                    out_file.write(reinterpret_cast<const char *>(worker.get_out_block()), compressed_block_size);
+                }
+            }
+        }
+
+        in_file.close();
+        out_file.close();
+
+        return 0;
+    }
+
+    template <typename HASH, typename T>
+    static int decompress_block(const std::string& path, const std::string& out_path, int k) {
+        std::ifstream in_file(path, std::ios::binary);
+        if (in_file.fail()) {
+            return 1;
+        }
+        std::ofstream out_file(out_path, std::ios::binary);
+
+        int core_number = get_cores() - 2;
+
+        // Needs max block size as the block size is read after the allocation of these buffers
+        int max_block_size = 1024 * 1024 * 10;
+
+        std::vector<MTFBlockWorker<HASH, T>> workers;
+        for (int i = 0; i < core_number; i++) {
+            workers.emplace_back(k, 256 * 256 * 256, max_block_size, max_block_size * 4 + 1024);
+        }
+
+        while (in_file.good()) {
+            for (MTFBlockWorker<HASH, T>& worker: workers) {
+                uint32_t block_size;
+                in_file.read(reinterpret_cast<char *>(&block_size), 4);
+
+                in_file.read(reinterpret_cast<char *>(worker.get_in_block()), block_size);
+                long read_bytes = in_file.gcount();
+
+                worker.startDecompression(read_bytes);
+            }
+
+            for (MTFBlockWorker<HASH, T>& worker: workers) {
+                uint32_t decompressed_block_size = worker.get();
+                if (decompressed_block_size > 0) {
+                    out_file.write(reinterpret_cast<const char *>(worker.get_out_block()), decompressed_block_size);
+                }
+            }
+        }
+
+        in_file.close();
+        out_file.close();
+
+        return 0;
+    }
+
+    template <typename HASH, typename T>
+    static int compress_stream(const std::string& path, const std::string& out_path, int k) {
+        std::ifstream in_file(path, std::ios::binary);
+        if (in_file.fail()) {
+            return 1;
+        }
+        ofbitstream out_file(out_path);
+        //std::ofstream out_file(out_path);
+
+        HASH hash(k, 4095);
+
+        MTFHashTableStream<T> mtf(k, 1024 * 1024, hash); // 1 MB block size
+        mtf.encode(in_file, out_file);
+
+        in_file.close();
+        out_file.close();
+
+        return 0;
+    }
+
+    template <typename HASH, typename T>
+    static int decompress_stream(const std::string& path, const std::string& out_path, int k) {
+        ifbitstream in_file(path);
+        //std::ifstream in_file(path);
+        if (in_file.fail()) {
+            return 1;
+        }
+        std::ofstream out_file(out_path, std::ios::binary);
+
+        HASH hash(k, 4095);
+        MTFHashTableStream<T> mtf(k, 1024 * 1024, hash); // 1 MB block size
+        mtf.decode(in_file, out_file);
+
+        in_file.close();
+        out_file.close();
+
+        return 0;
+    }
 };
 
 
